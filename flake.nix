ec1a4962762f36e1dbea625af47fab119234e0c1
raf <rraf@tuta.io>
2025-01-27 16:56:50 +0000

    chore: organising

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A Python development environment using Nix flakes.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.python3
+            pkgs.python3Packages.virtualenv
+          ];
+        };
+      }
+    );
+}
